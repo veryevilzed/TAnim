6d3eabc904bfb720fced928a5a5cbe48c8ea7073
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2018-03-06 21:11:26 +0200

    0.06

--- tanim.js
+++ tanim.js
@@ -1,4 +1,4 @@
 
-import TAnim, {Animation, Animator, State, Parallel} from './src/Global.js'
-export {Animation, Animator, State, Parallel}
+import TAnim, {TAnimation, TAnimator, TState, TParallel} from './src/Global.js'
+export {TAnimation, TAnimator, TState, TParallel}
 export default TAnim;
